ffa7ffce5d95a828c50702c5155fbfa9d772ceb7
Lewis Horwood <horwood.lewis@gmail.com>
2019-06-08 13:04:03 +0900

    modify server.js for client side routing

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
