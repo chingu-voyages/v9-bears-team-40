d91066ae210d9f451167728057cf5ce716208d18
Lewis Horwood <horwood.lewis@gmail.com>
2019-06-16 19:42:46 +0900

    theme App

---
+++ src/theme.tsx
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    bg: "#e2dadb",
+    fg: "#fdfdff",
+    brand: "#7b7263",
+    body: "#393d3f",
+    link: "#7ca982"
+  },
+  fonts: {
+    header: "",
+    body: ""
+  }
+};
