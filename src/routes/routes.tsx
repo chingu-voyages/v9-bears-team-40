f0d82f94d473cfc36282789953f892f2b6a0779b
SangilYun <nasuler@gmail.com>
2019-06-23 05:08:51 +0900

    add key property

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,18 +3,20 @@ import { Route } from "react-router-dom";
 
 import HomePage from "../Pages/HomePage";
 import BookPage from "../Pages/BookPage";
+import { RouteComponentProps } from "react-router";
+
+interface IKey {
+  key: string;
+}
 
 const Routes = () => (
   <React.Fragment>
     <Route exact path="/" component={HomePage} />
+    {/* <Route path="/b/:general" component={BookPage}/> */}
     <Route
       path="/b/:general"
       render={props => (
-        <BookPage
-          search={{
-            general: props.match.params.general
-          }}
-        />
+        <BookPage {...props} key={props.match.params.general}></BookPage>
       )}
     />
   </React.Fragment>
