ce715d3655a01ef88a5713b01f18375959b171b8
Lewis Horwood <horwood.lewis@gmail.com>
2019-06-23 13:27:43 +0900

    remove unused types

--- src/Components/Book.tsx
+++ src/Components/Book.tsx
@@ -1,26 +1,6 @@
 import * as React from "react";
 
-type BookProps = {
-  author?: string;
-  general?: string;
-  isbn?: string;
-  title?: string;
-};
-
-type BookState = {
-  error: boolean;
-  errorMessage: string | null;
-  loading: boolean;
-  book: {
-    title: string;
-    authors?: [string];
-    date?: string;
-    isbn?: string;
-    pages?: string;
-    description?: string;
-  };
-};
-
+//Todo: Create proper types and components for search results
 const Book = (props: any) => {
   return <h1>Book title : {props.title}</h1>;
 };
