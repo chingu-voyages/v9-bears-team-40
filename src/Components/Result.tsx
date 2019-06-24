248592391d26d2f8fc4842d636e4fecc50a508d0
SangilYun <nasuler@gmail.com>
2019-06-24 22:08:40 +0900

    add bookcover image

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -2,7 +2,12 @@ import * as React from "react";
 
 //Todo: Create proper types and components for search results
 const Result = (props: any) => {
-  return <h1>Book title : {props.title}</h1>;
+  return (
+    <div>
+      <img src={props.imageLinks.thumbnail} alt="bookcover"></img>
+      <h1>Book title : {props.title}</h1>
+    </div>
+  );
 };
 
 export default Result;
