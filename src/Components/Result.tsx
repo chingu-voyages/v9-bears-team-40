a6e9673a0e36f907680cedeb719d53c5579a5781
unknown <nasuler@gmail.com>
2019-06-26 11:42:25 +0900

    add components and athors array stringify method

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -1,12 +1,45 @@
 import * as React from "react";
+import styled from "../utils/theme";
+
+const SearchResultCard = styled.div`
+  display: flex;
+  // justify-content :center;
+  margin: 5px;
+`;
+
+const SearchResultDesc = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchResultImg = styled.img`
+  margin-right: 5px;
+`;
+const Title = styled.h1`
+  margin: 0;
+`;
+
+const Author = styled.h2`
+  margin: 0;
+`;
+
+function authorsArrayToString(arr: Array<string>) {
+  return arr.reduce((acc, curr, idx) => {
+    return idx === arr.length - 1 ? acc + " and " + curr : acc + ", " + curr;
+  }, arr.shift());
+}
 
 //Todo: Create proper types and components for search results
 const Result = (props: any) => {
+  let authors = authorsArrayToString(props.searchResultObjects.authors);
   return (
-    <div>
-      <img src={props.imageLinks.thumbnail} alt="bookcover"></img>
-      <h1>Book title : {props.title}</h1>
-    </div>
+    <SearchResultCard>
+      <SearchResultImg src={props.imageLinks.thumbnail} alt="bookcover" />
+      <SearchResultDesc>
+        <Title>{props.title}</Title>
+        <Author>by {authors}</Author>
+      </SearchResultDesc>
+    </SearchResultCard>
   );
 };
 
