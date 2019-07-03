24f35621b12438df0b5deb8401bc1ebd09991a0d
Lewis Horwood <horwood.lewis@gmail.com>
2019-07-03 21:21:31 +0900

    add prototypical book details page (needs more styling)

--- src/Components/BookDetails.tsx
+++ src/Components/BookDetails.tsx
@@ -1,14 +1,60 @@
 import * as React from "react";
 import styled from "../utils/theme";
 import googleBooksVolume from "../types/googleBooksVolume";
-import { props } from "bluebird";
+import authorsArrayToString from "../utils/authorsArrayToString";
+import Stars from "./Stars";
 
 type BookDetailsProps = {
   book: googleBooksVolume;
 };
 
+const Thumnbnail = styled.img`
+  height: auto;
+  width: 16rem;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > * {
+    margin: 0;
+  }
+`;
+
 const BookDetails: React.FunctionComponent<BookDetailsProps> = props => {
-  return <h1>{props.book.volumeInfo.title}</h1>;
+  const {
+    title,
+    authors,
+    publishedDate,
+    pageCount,
+    imageLinks,
+    industryIdentifiers,
+    averageRating,
+    ratingsCount
+  } = props.book.volumeInfo;
+
+  return (
+    <Wrapper>
+      <h1>{title}</h1>
+      {authors ? <h2>{authorsArrayToString(authors)}</h2> : ""}
+      {averageRating && ratingsCount ? (
+        <React.Fragment>
+          <Stars averageRating={averageRating.toString()} />{" "}
+          <span>({ratingsCount})</span>
+        </React.Fragment>
+      ) : null}
+      <span>{publishedDate ? publishedDate.slice(0, 4) : null}</span>
+      <span>{pageCount ? pageCount + " pages" : null}</span>
+      {imageLinks && imageLinks.thumbnail ? (
+        <Thumnbnail src={imageLinks.thumbnail} />
+      ) : null}
+      {industryIdentifiers ? (
+        <span>
+          {industryIdentifiers[0].type}: {industryIdentifiers[0].identifier}
+        </span>
+      ) : null}
+    </Wrapper>
+  );
 };
 
 export default BookDetails;
