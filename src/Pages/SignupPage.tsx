abb4b17e844ac660bb7949bb983017a8665326e0
SangilYun <nasuler@gmail.com>
2019-07-13 15:27:43 +0900

    add a user in signup page

--- src/Pages/SignupPage.tsx
+++ src/Pages/SignupPage.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import styled from "../utils/theme";
 import { Button } from "../Components/Button";
 
-const Form = styled.form`
+const InnderWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -46,35 +46,75 @@ const Wrapper = styled.div`
   width: 100%;
   >>>>>>>84e1887e65422d50cb7d04acd9f40705ccfd12c9: src/Components/Signup.tsx;
 `;
-
-const SignupPage = (props: any) => {
-  return (
-    <Wrapper>
-      <Button
-        style={{
-          position: "absolute",
-          top: 0,
-          right: 0
-        }}
-        onClick={props.toggleModal}
-      >
-        <i className="fas fa-times" />
-      </Button>
-      <Form action="database-endpoint">
-        <h1>Sign Up</h1>
-        <FormFields>
-          <label>Name:</label>
-          <input />
-          <label>Email:</label>
-          <input />
-          <label>Password:</label>
-          <input type="password" />
-        </FormFields>
-
-        <Button>Sign up</Button>
-      </Form>
-    </Wrapper>
-  );
+type IProps = {
+  toggleModal: any;
 };
+type IState = {
+  email: string;
+  name: string;
+  password: string;
+};
+
+class SignupPage extends React.Component<IProps, IState> {
+  state = {
+    email: "",
+    name: "",
+    password: ""
+  };
+
+  onEmailChange = (event: any) => {
+    this.setState({ email: event.target.value });
+  };
+
+  onNameChange = (event: any) => {
+    this.setState({ name: event.target.value });
+  };
+
+  onPasswordChange = (event: any) => {
+    this.setState({ password: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3000/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    }).then(this.props.toggleModal());
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Button
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0
+          }}
+          onClick={this.props.toggleModal}
+        >
+          <i className="fas fa-times" />
+        </Button>
+        <InnderWrapper>
+          <h1>Sign Up</h1>
+          <FormFields>
+            <label>Name:</label>
+            <input onChange={this.onNameChange} />
+            <label>Email:</label>
+            <input onChange={this.onEmailChange} />
+            <label>Password:</label>
+            <input onChange={this.onPasswordChange} type="password" />
+          </FormFields>
+
+          <Button onClick={this.onSubmitSignIn}>Sign up</Button>
+        </InnderWrapper>
+      </Wrapper>
+    );
+  }
+}
 
 export default SignupPage;
